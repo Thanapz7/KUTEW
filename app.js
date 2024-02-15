14d39226fffea99fd7645022a7451f185f98a556
thanapz <thanaphon.phet@ku.th>
2024-02-15 23:36:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//show hidden pass
+const eyeIcon = document.querySelector('#login-eye');
+const passwordEl = document.querySelector('#login-pass');
+
+eyeIcon.addEventListener("click", () => {
+    if (eyeIcon.classList.contains("fa-eye-slash")) {
+        eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
+        passwordEl.setAttribute("type", "password");
+    } else {
+        eyeIcon.classList.replace("fa-eye", "fa-eye-slash");
+        passwordEl.setAttribute("type", "text");
+    }
+});
