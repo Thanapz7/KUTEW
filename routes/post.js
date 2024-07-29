98f95a2beacb27452a9a75b46d4a70a968851e42
Tadchai <Tadpmr@gmail.com>
2024-07-29 14:23:10 +0700

    Add api getpostbyid

--- routes/post.js
+++ routes/post.js
@@ -7,16 +7,19 @@ const {
     updatePost,
     deletePost,
     searchPost,
-    getMyPost
+    getMyPost,
+    getPostById
     
 
   } =require("../controllers/post.js");
   router.get("/", getAllPost);
+  router.get("/:id", getPostById);
   router.post("/", insertPost);
   router.put("/:id", updatePost);
   router.delete("/:id", deletePost);
   router.get("/search", searchPost);
   router.get("/myp", getMyPost);
+  
   
   
   
