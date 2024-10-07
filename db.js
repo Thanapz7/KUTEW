095b92366e25380668301835099505a86aadabc6
Tadchai <Tadpmr@gmail.com>
2024-10-07 23:52:30 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+// สร้าง connection db แทนการใช้ createConnection
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -12,18 +13,21 @@ const db = mysql.createConnection({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    ssl      : {
+    ssl: {
         rejectUnauthorized: false // ใช้ถ้า SSL จำเป็น
-      }
+    }
 });
 
-
-db.connect(err => {
+// ทดสอบการเชื่อมต่อจาก db
+db.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to MySQL:', err);
         return;
     }
     console.log('Connected to MySQL');
+
+    // ปล่อย connection กลับไปที่ db หลังจากใช้งานเสร็จ
+    connection.release();
 });
 
 module.exports = db;
